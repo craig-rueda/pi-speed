569ee8461872390c2d965e2dfe638a13230bebf7
Craig <craig@craigrueda.com>
2020-10-11 20:36:36 -0700

    Updating tests

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -13,25 +13,25 @@ class MonitorTest(TestCase):
     def test_single_frame_message(self):
         self._monitor.on_ws_message(self._read_sample("single_frame_message.txt"))
         self._monitor._update_output.assert_called_once()
-        self._monitor._update_output.assert_called_with(27296, 27832, '99.152.117.124')
+        self._monitor._update_output.assert_called_with(27296, 27832, '13.123.123.123')
 
     def test_single_frame_multi_message(self):
         self._monitor.on_ws_message(self._read_sample("single_frame_multi_message.txt"))
         self._monitor._update_output.assert_has_calls(
-            [call(27296, 27832, '99.152.117.124'), call(27304, 27760, '99.152.117.124')]
+            [call(27296, 27832, '13.123.123.123'), call(27304, 27760, '13.123.123.123')]
         )
 
     def test_multiple_frame_message(self):
         self._monitor.on_ws_message(self._read_sample("multi_frame_message_1.txt"))
         self._monitor.on_ws_message(self._read_sample("multi_frame_message_2.txt"))
         self._monitor._update_output.assert_called_once()
-        self._monitor._update_output.assert_called_with(1978872, 208192, '99.152.117.124')
+        self._monitor._update_output.assert_called_with(1978872, 208192, '13.123.123.123')
 
     def test_multiple_frame_with_naked_header(self):
         self._monitor.on_ws_message(self._read_sample("multi_frame_message_naked_1.txt"))
         self._monitor.on_ws_message(self._read_sample("multi_frame_message_naked_2.txt"))
         self._monitor._update_output.assert_called_once()
-        self._monitor._update_output.assert_called_with(570848, 51624, '99.152.117.124')
+        self._monitor._update_output.assert_called_with(570848, 51624, '13.123.123.123')
 
     def test_human_rates(self):
         self.assertEqual(self._monitor._human_bps(123), "123 bps")
